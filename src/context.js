52792b6484be1d2bfcc06dae3f2740d85901fa27
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-05-15 02:58:18 -0700

    done with setting up useContext

--- src/context.js
+++ src/context.js
@@ -1,1 +1,12 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext } from 'react';
+// AppContext and AppProvider
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+   return <AppContext.Provider value="Hello">{children}</AppContext.Provider>;
+};
+export const useGlobalContext = () => {
+   return useContext(AppContext);
+};
+export { AppContext, AppProvider };
